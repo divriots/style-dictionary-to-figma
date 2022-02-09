092a7fbb33c02223f0d2e34421d685a4a45dd62d
jorenbroekema <joren.broekema@gmail.com>
2022-02-09 14:44:33 +0100

    chore: prepare first release

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,3 @@
 module.exports = {
   extends: ['@open-wc/eslint-config', 'eslint-config-prettier'].map(require.resolve),
-  overrides: [
-    {
-      files: ['**/*.js'],
-      rules: {
-        'import/no-unresolved': 'off', // eslint not smart enough atm to understand package exports maps
-      },
-    },
-  ],
 };
