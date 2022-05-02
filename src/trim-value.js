763acc93bd708fa6ffcfd53a44c5d056c65d11c2
Mihkel Eidast <mihkel@sixfold.com>
2022-05-02 16:30:58 +0300

    fix: allow trimValue to work with values shaped like objects

--- src/trim-value.js
+++ src/trim-value.js
@@ -4,17 +4,22 @@
 
 /**
  * @param {Obj} obj
+ * @param {boolean} isValueObj
  * @returns {Obj}
  */
-export function trimValue(obj) {
+export function trimValue(obj, isValueObj = false) {
   const newObj = { ...obj };
   Object.keys(newObj).forEach(key => {
-    if (key === 'value') {
-      const val = /** @type {string} */ (newObj[key]);
-      const reg = /^\{(.*)\}$/g;
-      const matches = reg.exec(val);
-      if (matches && matches[1]) {
-        newObj[key] = val.replace('.value', '');
+    if (key === 'value' || isValueObj) {
+      if (typeof newObj[key] === 'string') {
+        const val = /** @type {string} */ (newObj[key]);
+        const reg = /^\{(.*)\}$/g;
+        const matches = reg.exec(val);
+        if (matches && matches[1]) {
+          newObj[key] = val.replace('.value', '');
+        }
+      } else if (typeof newObj[key] === 'object') {
+        newObj[key] = trimValue(/** @type {Obj} */ (newObj[key]), true);
       }
     } else if (typeof newObj[key] === 'object') {
       newObj[key] = trimValue(/** @type {Obj} */ (newObj[key]));
