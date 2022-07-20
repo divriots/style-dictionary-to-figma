b6cb742efd0815f32a96bb306125460d2068a251
Joren Broekema <joren.broekema@gmail.com>
2022-07-20 08:37:43 +0200

    fix: use ref value to only target and restore reference values (#21)

--- src/use-ref-value.js
+++ src/use-ref-value.js
@@ -1,6 +1,27 @@
 /**
  * @typedef {import('./style-dictionary-to-figma.js').Obj} Obj
+ * @typedef {string|Object|Array<unknown>|number} Value
  */
+
+/**
+ * @param {Value} val
+ * @returns {boolean}
+ */
+function isRefValue(val) {
+  if (Array.isArray(val)) {
+    return val.some(prop => isRefValue(/** @type {Value} */ (prop)));
+  }
+
+  if (typeof val === 'string') {
+    return val.startsWith('{') && val.endsWith('}');
+  }
+
+  if (typeof val === 'number') {
+    return false;
+  }
+
+  return Object.values(val).some(prop => isRefValue(prop));
+}
 
 /**
  * @param {Obj} obj
@@ -10,7 +31,10 @@ export function useRefValue(obj) {
   const newObj = { ...obj };
   Object.keys(newObj).forEach(key => {
     if (key === 'original' && !newObj.ignoreUseRefValue) {
-      newObj.value = /** @type {Obj} */ (newObj[key]).value;
+      const originalValue = /** @type {string} */ (/** @type {Obj} */ (newObj[key]).value);
+      if (isRefValue(originalValue)) {
+        newObj.value = /** @type {Obj} */ (newObj[key]).value;
+      }
     } else if (typeof newObj[key] === 'object') {
       const newValue = useRefValue(/** @type {Obj} */ (newObj[key]));
       newObj[key] = Array.isArray(newObj[key]) ? Object.values(newValue) : newValue;
